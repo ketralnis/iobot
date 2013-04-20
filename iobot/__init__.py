6e63e26847bd4919551760bbf54c1c380c556cb6
Mark McGuire <mark.b.mcg@gmail.com>
2013-04-20 22:50:35 +0000

    Split out IrcEvent
    - split out IrcEvent to separate file

--- iobot/__init__.py
+++ iobot/__init__.py
@@ -8,6 +8,7 @@ import logging
 from tornado.ioloop import IOLoop
 from tornado.iostream import IOStream
 
+from iobot.event import IrcEvent
 from iobot.plugins import CommandRegister, TextPlugin
 
 logger = logging.getLogger(__name__)
@@ -22,67 +23,6 @@ class IrcProtoCmd(object):
         self.actn(irc)
         for h in self.hooks:
             h(irc)
-
-renick = re.compile("^(\w*?)!")
-
-class IrcEvent(object):
-    """
-    tries to guess and populate something from an ircd statement
-    """
-    EVENT_PATTERN = re.compile((r'^(:(?P<prefix>((?P<nick>[^!]+)!'
-            r'(?P<user>[^@]+)@(?P<host>[^ ]+)|[^ ]+)) )?'
-            r'((?P<numeric>[0-9]{3})|(?P<command>[^ ]+))'
-            r'( (?P<destination>[^:][^ ]*))?( :(?P<text>.*)|'
-            r' (?P<parameters>.*))?$'))
-    COMMAND_REGEX = (r'^{}(?P<command>[^ ]*)'
-            r'( (?P<params>.*))?$')
-
-    def __init__(self, bot, raw=None):
-        self._bot = bot
-        self.raw = raw
-        self.type = None
-        self.origin = None
-        self.destination = None
-        self.text = None
-        self.parameters = []
-        self.parameters_raw = None
-        self.command = None
-        self.command_params = None
-        if raw:
-            self._parse()
-
-    def __repr__(self):
-        return ('<IrcEvent object: %s %s>' % (self.type, self.destination))
-
-    def _parse(self):
-        if self.raw[-2:] == '\r\n':
-            line = self.raw[:-2]
-        else:
-            line = self.raw
-        m = self.EVENT_PATTERN.match(line)
-        self.origin = m.group('prefix')
-        self.type = m.group('command') or m.group('numeric')
-        self.destination = m.group('destination')
-        self.nick = m.group('nick')
-        self.user = m.group('user')
-        self.host = m.group('host')
-        self.text = m.group('text')
-        self.parameters_raw = m.group('parameters')
-        self.parameters = (self.parameters_raw.split() if
-                self.parameters_raw else [])
-
-        if self.type == 'PRIVMSG':
-            self._parse_command()
-
-    def _parse_command(self):
-        regex = self.COMMAND_REGEX.format(self._bot.char)
-        m = re.match(regex, self.text)
-        if m:
-            self.command = m.group('command')
-            self.command_params = m.group('params')
-
-    def reply(self, text):
-        self._bot.private_message(self.destination, text)
 
 class IOBot(object):
     def __init__(
---
+++ iobot/event.py
@@ -0,0 +1,60 @@
+import re
+
+class IrcEvent(object):
+    """
+    tries to guess and populate something from an ircd statement
+    """
+    EVENT_PATTERN = re.compile((r'^(:(?P<prefix>((?P<nick>[^!]+)!'
+            r'(?P<user>[^@]+)@(?P<host>[^ ]+)|[^ ]+)) )?'
+            r'((?P<numeric>[0-9]{3})|(?P<command>[^ ]+))'
+            r'( (?P<destination>[^:][^ ]*))?( :(?P<text>.*)|'
+            r' (?P<parameters>.*))?$'))
+    COMMAND_REGEX = (r'^{}(?P<command>[^ ]*)'
+            r'( (?P<params>.*))?$')
+
+    def __init__(self, bot, raw=None):
+        self._bot = bot
+        self.raw = raw
+        self.type = None
+        self.origin = None
+        self.destination = None
+        self.text = None
+        self.parameters = []
+        self.parameters_raw = None
+        self.command = None
+        self.command_params = None
+        if raw:
+            self._parse()
+
+    def __repr__(self):
+        return ('<IrcEvent object: %s %s>' % (self.type, self.destination))
+
+    def _parse(self):
+        if self.raw[-2:] == '\r\n':
+            line = self.raw[:-2]
+        else:
+            line = self.raw
+        m = self.EVENT_PATTERN.match(line)
+        self.origin = m.group('prefix')
+        self.type = m.group('command') or m.group('numeric')
+        self.destination = m.group('destination')
+        self.nick = m.group('nick')
+        self.user = m.group('user')
+        self.host = m.group('host')
+        self.text = m.group('text')
+        self.parameters_raw = m.group('parameters')
+        self.parameters = (self.parameters_raw.split() if
+                self.parameters_raw else [])
+
+        if self.type == 'PRIVMSG':
+            self._parse_command()
+
+    def _parse_command(self):
+        regex = self.COMMAND_REGEX.format(self._bot.char)
+        m = re.match(regex, self.text)
+        if m:
+            self.command = m.group('command')
+            self.command_params = m.group('params')
+
+    def reply(self, text):
+        self._bot.private_message(self.destination, text)
